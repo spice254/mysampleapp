511c73b88d85d9090e3b10554cad5ca6de857c3f
spice254 <jamospice@gmail.com>
2021-01-11 18:37:39 +0300

    home page activation
    tweaked the views

---
+++ stockmgmt/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Stock
+from .form import StockCreateForm
+
+
+# Register your models here.
+class StockCreateAdmin(admin.ModelAdmin):
+   list_display = ['category', 'item_name', 'quantity']
+   form = StockCreateForm
+   list_filter = ['category']
+   search_fields = ['category', 'item_name']
+
+admin.site.register(Stock, StockCreateAdmin)
+
---
+++ stockmgmt/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+	title = 'Welcome: This is the Home Page'
+	context = {
+	"title": title,
+	}
+	return render(request, "home.html",context)
