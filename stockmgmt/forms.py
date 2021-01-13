2d020542966f5018280378faadb9f1d837b0564f
spice254 <jamospice@gmail.com>
2021-01-13 17:51:20 +0300

    dublication deactivated

--- stockmgmt/forms.py
+++ stockmgmt/forms.py
@@ -1,13 +1,31 @@
 from django import forms
 
-from .models import Stock
+from . models import Stock
 
 class StockCreateForm(forms.ModelForm):
-   class Meta:
-     model = Stock
-     fields = ['category', 'item_name', 'quantity']
+	class Meta:
+		model = Stock
+		fields = ['category', 'item_name', 'quantity']
+
+	def clean_category(self):
+		category = self.cleaned_data.get('category')
+		if not category:
+			raise forms.ValidationError('This field is required')
+
+		
+		return category
+
+
+	def clean_item_name(self):
+		item_name = self.cleaned_data.get('item_name')
+		if not item_name:
+			raise forms.ValidationError('This field is required')
+		for instance in Stock.objects.all():
+			if instance.item_name == item_name:
+				raise forms.ValidationError(item_name + ' is already created')
+		return item_name  
 
 class StockSearchForm(forms.ModelForm):
-   class Meta:
-     model = Stock
-     fields = ['category', 'item_name']
+  class Meta:
+    model = Stock
+    fields = ['category', 'item_name']
